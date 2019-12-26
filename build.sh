7823e0396433ebb99c0cb6615026db42f84cf642
Finbarrs Oketunji <oketunjifinbarrs@gmail.com>
2019-12-25 16:15:46 -0800

    app image updated
    Signed-off-by: Finbarrs Oketunji <oketunjifinbarrs@gmail.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 #Linux x64
-nativefier -p linux -a x64 -i ./og-image.png --name "NotJustOk" "https://my.notjustok.com/westafrica"
+nativefier -p linux -a x64 -i ./og-image.png --name "Pandora" "https://www.pandora.com/"
 
 #Windows
-nativefier -p windows -i ./og-image.png --name "NotJustOk" "https://my.notjustok.com/westafrica"
+nativefier -p windows -i ./og-image.png --name "Pandora" "https://www.pandora.com/"
 
 #macOS
-nativefier -i ./og-image.png --name "NotJustOk" "https://my.notjustok.com/westafrica"
+nativefier -i ./og-image.png --name "Pandora" "https://www.pandora.com/"
