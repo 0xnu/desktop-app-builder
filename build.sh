0b11341c17585898131b8d55e572a0188e25331e
Finbarrs Oketunji <oketunjifinbarrs@gmail.com>
2019-12-25 16:27:55 -0800

    app image updated
    Signed-off-by: Finbarrs Oketunji <oketunjifinbarrs@gmail.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 #Linux x64
-nativefier -p linux -a x64 -i ./og-image.png --name "Pandora" "https://www.pandora.com/"
+nativefier -p linux -i ./og-image.png -n "Pandora" "https://www.pandora.com/"
 
 #Windows
-nativefier -p windows -i ./og-image.png --name "Pandora" "https://www.pandora.com/"
+nativefier -p windows -i ./og-image.png -n "Pandora" "https://www.pandora.com/"
 
 #macOS
-nativefier -i ./og-image.png --name "Pandora" "https://www.pandora.com/"
+nativefier -p osx -i ./og-image.png -n "Pandora" "https://www.pandora.com/"
